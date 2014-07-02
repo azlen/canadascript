d6e4c496c2581130d86bf0cdc6261c7c9b99ea36
azlen <azlen@livingcode.org>
2014-07-02 14:07:08 -0700

    added functions

--- parser.js
+++ parser.js
@@ -9,8 +9,13 @@ window.onload = function(){
 		var script = '';
 		for(var e = 0; e < inner.length; e++){
 			if(inner[e].match(/.*=.* eh\?/)){
-				var varname = inner[e].match(/.+=/)[0].replace(/ |=/g,'');
-				var t = varname+' = '+inner[e].match(/=.+ /)[0].replace(/ |=/g,'')+';\n';
+				//CREATES VARIABLE
+				//bacon = 'hello world' eh?
+
+				//var bacon = 'hello world';
+
+				var varname = inner[e].match(/^[\w-]+/)[0]
+				var t = varname+' = '+inner[e].match(/'.*'/)+';\n'; //FIX THIS... VARIABLES CURRENTLY ONLY CAN BE STRINGS
 				if(variables.indexOf(varname) != -1){
 					script+=t;
 				}else{
@@ -18,18 +23,61 @@ window.onload = function(){
 					script+='var '+t;
 				}
 			}else if(inner[e].match(/whadda'yat .+\?/)){
-				var t = 'console.log('+inner[e].match(/ .+\?/)[0].replace(/ |\?/g,'')+');\n';
+				//CONSOLE LOG
+				// whadda'yat bacon?
+
+				// console.log(bacon);
+
+				var t = 'console.log('+inner[e].match(/ [\w-]+.*\?/)[0].match(/[a-zA-Z0-9'].*/)[0].slice(0, - 1)+');\n';
 				script += t;
-			}else if(inner[e].match(/oot '.+ aboot .+/)){
-				console.log(inner[e].match(/'.+ a/)[0]);
-				var varname = inner[e].match(/'.+ a/)[0].replace(/'| |a/g, '');
-				var t = 'for('+varname+' = 0; '+varname+' < '+inner[e].match(/boot .+/)[0].replace(/b|o|t| /g,'')+'; '+varname+'++){\n';
+			}else if(inner[e].match(/oot '.+ aboot .+:/)){
+				var varname = inner[e].match(/'[\w-]+/)[0].match(/[\w-]+$/)[0];
+				var t = 'for('+varname+' = 0; '+varname+' < '+inner[e].match(/[\w-]+:$/)[0].slice(0, - 1)+'; '+varname+'++){\n';
 				script += t;
 			}else if(inner[e].match(/sorry/)){
+				//CLOSES FUNCTIONS, FORS, IFS, ETC..
+				// sorry
+
+				// }
+
 				script += '}\n'
+			}else if(inner[e].match(/O .+, .+:/)){
+				//CREATES FUNCTION
+				// O Canada, our home and native land:
+
+				// function Canada(our, home, and native land){
+
+				var t = 'function '+inner[e].match(/\s+[^\s]+/)[0].match(/[\w-]+/)[0]+'('+inner[e].match(/, .+:/)[0].match(/[a-zA-Z0-9][a-zA-Z0-9 ]+/)[0].split(' ').join(', ')+'){\n';
+				script += t;
+			}else if(inner[e].match(/O .+:/)){
+				//CREATES FUNCTION
+				// O Canada:
+
+				// function Canada(){
+
+				var t = 'function '+inner[e].match(/\s+[^\s]+/)[0].match(/[\w-]+/)[0]+'(){\n';
+				script += t;
+			}else if(inner[e].match(/O .+, .+[.|!]/)){
+				//CALLS FUNCTION
+				// O Canada, our home and native land!
+				// O Canada, our home and native land.
+
+				// Canada(our, home, and, native, land);
+
+				var t = inner[e].match(/\s+[^\s]+/)[0].match(/[\w-]+/)[0]+'('+inner[e].match(/, .+[.|!]/)[0].match(/[a-zA-Z0-9'][a-zA-Z0-9 ']*/)[0].split(' ').join(', ')+');\n';
+				script += t;
+			}else if(inner[e].match(/O .+[.|!]/)){
+				//CALLS FUNCTION
+				// O Canada!
+				// O Canada.
+
+				// Canada();
+
+				var t = inner[e].match(/\s+[^\s]+/)[0].match(/[\w-]+/)[0]+'();\n';
+				script += t;
 			}
 		}
-		console.log(script);
+		//console.log(script);
 		eval(script);
 	}
 }
